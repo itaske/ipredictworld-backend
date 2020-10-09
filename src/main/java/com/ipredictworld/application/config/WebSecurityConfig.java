bfee31be3ae6a89fc36cc0a535edb49647a7d245
Java-backend-developer-SSS <udochukwupatric@gmail.com>
2020-10-09 14:45:12 +0100

    Remove Cors

--- src/main/java/com/ipredictworld/application/config/WebSecurityConfig.java
+++ src/main/java/com/ipredictworld/application/config/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -58,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.csrf().disable()
                 .authorizeRequests()
         .antMatchers("/",
                 "/favicon.ico",
