5da225629e2a13177a7099e8104fe70b90d7f337
itaske <39966884+itaske@users.noreply.github.com>
2020-10-10 19:18:51 +0100

    Update UserAPI.java

--- src/main/java/com/ipredictworld/application/api/UserAPI.java
+++ src/main/java/com/ipredictworld/application/api/UserAPI.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
